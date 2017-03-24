41b1a29bd656f04915a88dec9d0d74041910985f
RallyKolibarova <sysprogram@abv.bg>
2017-03-24 22:55:50 +0200

    Add files via upload

---
+++ LastТhreeConsecutiveEqualStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastТhreeConsecutiveEqualStrings
+{
+    class LastТhreeConsecutiveEqualStrings
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+
+            int count = 1;
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if(array[i] == array[i - 1])
+                {
+                    count++;
+                    if(count == 3)
+                    {
+                        Console.WriteLine("{0} {0} {0}", array[i]);
+                        break;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+        }
+    }
+}
